64239a21e10866554ffca45290e6ab585285adc2
Misaki Shibutani <231205095@ccmailg.meijo-u.ac.jp>
2024-09-05 17:32:05 +0900

    [add] #3 デザイン作成

--- app/src/main/java/jp/ac/meijou/s231205036/android/schedulestrengthcalendar/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/s231205036/android/schedulestrengthcalendar/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //hhhhh
         super.onCreate(savedInstanceState);
         db = FirebaseFirestore.getInstance();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
