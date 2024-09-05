86fbaf644dfc3cddd9dc4f7c986b9c9fe9d8fa10
shun <shun01.oh@gmail.com>
2024-09-05 15:11:37 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/meijou/s231205036/android/schedulestrengthcalendar/MainActivity.java
@@ -0,0 +1,51 @@
+package jp.ac.meijou.s231205036.android.schedulestrengthcalendar;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+
+    public void saveData() {
+        // 保存するデータを作成
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // "users" コレクションにドキュメントを追加
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    System.out.println("DocumentSnapshot added with ID: " + documentReference.getId());
+                })
+                .addOnFailureListener(e -> {
+                    System.err.println("Error adding document: " + e.getMessage());
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        saveData();
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
