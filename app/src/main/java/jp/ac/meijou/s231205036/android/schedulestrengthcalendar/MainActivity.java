4536a45f352db67812963fa70d0a5d14dac5c8ce
shun <shun01.oh@gmail.com>
2024-09-05 17:23:09 +0900

    [add] #1 初期設定の変更

--- app/src/main/java/jp/ac/meijou/s231205036/android/schedulestrengthcalendar/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/s231205036/android/schedulestrengthcalendar/MainActivity.java
@@ -17,38 +17,27 @@ import jp.ac.meijou.s231205036.android.schedulestrengthcalendar.databinding.Acti
 public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private ActivityMainBinding binding;
-
-    public void saveData() {
-        // 保存するデータを作成
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        // "users" コレクションにドキュメントを追加
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(documentReference -> {
-                    System.out.println("DocumentSnapshot added with ID: " + documentReference.getId());
-                })
-                .addOnFailureListener(e -> {
-                    System.err.println("Error adding document: " + e.getMessage());
-                });
-    }
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
-        saveData();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        prefDataStore = PrefDataStore.getInstance(this);
 
+        // 保存されたユーザー名を取得して表示
+        String savedUser = prefDataStore.getString("name").orElse("");
+        String saveFlag = prefDataStore.getString("flag").orElse("");
+        binding.textView.setText(saveFlag);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding.button.setOnClickListener(view -> {
+            var user = binding.editTextText.getText().toString();
+            var flag = "0";
+            prefDataStore.setString("name", user);
+            prefDataStore.setString("flag",flag);
         });
     }
+
 }
